5d6afcc7449c34a097d53cd6657e7e63c7b15021
Kaur2112 <67204767+Kaur2112@users.noreply.github.com>
2021-04-27 20:14:07 -0400

    Add files via upload
    added js file

---
+++ demo/src/main/resources/static/main.js
@@ -0,0 +1,34 @@
+console.log("It Works")
+
+$(document).ready(function () {
+    $('.btn').click(function(event) {
+        console.log('Clicked button')
+        var email = $('.email').val()
+        var name = $('.name').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if (email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>Email is valid<\div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid<\div>')
+        }
+
+        if (name.length > 2) {
+            statusElm.append('<div>Name is valid<\div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Name is not valid<\div>')
+        }
+
+        if (message.length > 1) {
+            statusElm.append('<div>Message is valid<\div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid<\div>')
+        }
+
+    })
+})
